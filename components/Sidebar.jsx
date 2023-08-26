4e29ac5431b30f5dbbb3f43e535ceee4f699082c
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-08-26 12:02:47 +0600

    complete sidebar design with submenu

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { RxSketchLogo, RxDashboard, RxPerson } from 'react-icons/rx';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { FiSettings } from 'react-icons/fi';
-import { AiOutlineSlack,AiTwotoneSnippets, AiOutlineCaretDown } from 'react-icons/ai';
+import { AiOutlineSlack,AiTwotoneSnippets, AiOutlineCaretDown, AiFillThunderbolt } from 'react-icons/ai';
 
 const Sidebar = ({ children }) => {
+  const [showSettingsSubmenu, setShowSettingsSubmenu] = React.useState(false);
+  const [showPayrollSubmenu, setShowPayrollSubmenu] = useState(false);
+  const [showReportSubmenu, setShowReportSubmenu] = useState(false);
+
   return (
     <div className='flex'>
       <div className='fixed w-20 md:w-60 h-screen p-2 bg-[#000] text-white border-r-[1px] flex flex-col justify-between'>
@@ -35,39 +39,90 @@ const Sidebar = ({ children }) => {
             </div>
           </Link>
           <Link href='/orders'  className=' hover:bg-green-500 cursor-pointer rounded-lg w-full'>
-            <div className=' hover:bg-gray-200 cursor-pointer my-2 p-1 rounded-lg inline-block'>
+            <div className=' cursor-pointer my-2 p-1 rounded-lg inline-block'>
             <div className="md:flex space-x-1">
               <HiOutlineShoppingBag size={20} /> <span className="text-sm font-bold hidden md:inline-block">Dashboard</span>
             </div>
             </div>
           </Link>
-          <Link href='/'  className='hover:bg-green-500 cursor-pointer rounded-lg w-full'>
-            <div className='  cursor-pointer my-2 p-1 rounded-lg inline-block'>
-            <div className="md:flex space-x-1">
-              <FiSettings size={20} /> <span className="text-sm font-bold hidden md:inline-block">Settings</span>
+
+          <div className="w-full -my-2"  onClick={() => setShowSettingsSubmenu(!showSettingsSubmenu)} >
+              <div className='hover:bg-green-500 cursor-pointer rounded-lg w-full '>
+                <div className='flex cursor-pointer my-2 px-1 py-3 rounded-lg justify-between'>
+                <div className="md:flex space-x-1">
+                  <FiSettings size={20} /> <span className="text-sm font-bold hidden md:inline-block">Settings</span>
+                  </div>
+                  <AiOutlineCaretDown className=''/>
+                </div>
               </div>
-            </div>
-          </Link>
-          <Link href='/'  className='hover:bg-green-500 cursor-pointer rounded-lg w-full'>
-            <div className=' flex cursor-pointer my-2 p-1 rounded-lg justify-between'>
-            <div className="md:flex space-x-1">
-              <AiOutlineSlack size={20} /> <span className="text-sm font-bold hidden md:inline-block">Payroll</span>
+
+                <div className="hover:bg-none">
+                  {showSettingsSubmenu && (
+                  <div className="submenu  ml-3">
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                  </div>
+                )}
               </div>
-              <AiOutlineCaretDown className=''/>
-            </div>
           
-          </Link>
-          {/* <div className="flex">
-              <AiOutlineSlack size={20} /> <span className="text-sm font-bold hidden md:inline-block">Payroll</span>
-              </div> */}
-          <Link href='/'  className='hover:bg-green-500 cursor-pointer rounded-lg w-full'>
-            <div className='  cursor-pointer my-2 p-1 rounded-lg flex justify-between'>
-            <div className="md:flex space-x-1">
-              <AiTwotoneSnippets size={20} /> <span className="text-sm font-bold hidden md:inline-block">Reports</span>
+          </div>
+
+          <div className="w-full -my-2"  onClick={() => setShowPayrollSubmenu(!showPayrollSubmenu)} >
+              <div className='hover:bg-green-500 cursor-pointer rounded-lg w-full '>
+                <div className='flex cursor-pointer my-2 px-1 py-3 rounded-lg justify-between'>
+                <div className="md:flex space-x-1">
+                  <AiOutlineSlack size={20} /> <span className="text-sm font-bold hidden md:inline-block">Payroll</span>
+                  </div>
+                  <AiOutlineCaretDown className=''/>
+                </div>
               </div>
-              <AiOutlineCaretDown className=''/>
-            </div>
-          </Link>
+
+                <div className="hover:bg-none">
+                  {showPayrollSubmenu && (
+                  <div className="submenu  ml-3">
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                  </div>
+                )}
+              </div>
+          
+          </div>
+
+
+          <div className="w-full -my-2"  onClick={() => setShowReportSubmenu(!showReportSubmenu)} >
+              <div className='hover:bg-green-500 cursor-pointer rounded-lg w-full '>
+                <div className='flex cursor-pointer my-2 px-1 py-3 rounded-lg justify-between'>
+                <div className="md:flex space-x-1">
+                  <AiTwotoneSnippets size={20} /> <span className="text-sm font-bold hidden md:inline-block">Reports</span>
+                  </div>
+                  <AiOutlineCaretDown className=''/>
+                </div>
+              </div>
+
+                <div className="hover:bg-none">
+                  {showReportSubmenu && (
+                  <div className="submenu  ml-3">
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                    <Link href='/' className="flex space-x-1 p-2 hover:bg-green-500 cursor-pointer rounded-lg w-full">
+                      <AiFillThunderbolt className='text-lg'/> <span className='text-sm font-bold'>SubMenu</span>
+                      </Link>
+                  </div>
+                )}
+              </div>
+          
+          </div>
+
+          
         </div>
       </div>
       <main className='ml-20 w-full'>{children}</main>
